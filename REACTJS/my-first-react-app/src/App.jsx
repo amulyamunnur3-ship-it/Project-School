042bb1b6c9a5616a712dfcc7a3589b4f971395dd
amulyamunnur3-ship-it <amulyamunnur3@gmail.com>
2026-07-22 12:47:14 +0530

    added reactjs

---
+++ REACTJS/my-first-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Student from "./Student";
+
+function App() {
+  return (
+    <div>
+      <h1>College Portal</h1>
+
+      <Student />
+    </div>
+  );
+}
+
+export default App;
---
+++ REACTJS/my-first-react-app/src/student.jsx
@@ -0,0 +1,11 @@
+function Student() {
+  return (
+    <div>
+      <h2>Student Details</h2>
+      <p>Name: Amulya</p>
+      <p>Course: AIML</p>
+    </div>
+  );
+}
+
+export default Student;
