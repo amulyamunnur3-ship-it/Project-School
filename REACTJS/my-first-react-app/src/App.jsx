668f4301fe111116cf1c1e3e22ddc0962c4b1f0f
amulyamunnur3-ship-it <amulyamunnur3@gmail.com>
2026-07-28 11:30:39 +0530

    updated employee component and app.jsx

--- REACTJS/my-first-react-app/src/App.jsx
+++ REACTJS/my-first-react-app/src/App.jsx
@@ -1,11 +1,41 @@
-import Student from "./Student";
+import Employee from "./Employee";
 
 function App() {
   return (
     <div>
-      <h1>College Portal</h1>
+      <h1>Employee Profile Cards</h1>
 
-      <Student />
+      <Employee 
+        name="Ramesh"
+        id="EMP101"
+        department="AIML"
+        designation="Assistant Professor"
+        salary="₹55,000"
+      />
+
+      <Employee 
+        name="Kavya"
+        id="EMP102"
+        department="CSE"
+        designation="Associate Professor"
+        salary="₹62,000"
+      />
+
+      <Employee 
+        name="Arjun"
+        id="EMP103"
+        department="ECE"
+        designation="Professor"
+        salary="₹70,000"
+      />
+
+      <Employee 
+        name="Sneha"
+        id="EMP104"
+        department="IT"
+        designation="Lecturer"
+        salary="₹45,000"
+      />
     </div>
   );
 }
