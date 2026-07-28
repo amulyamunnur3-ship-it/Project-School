a43f53d9d4b99119a327d9a780281a91cc9dfe43
amulyamunnur3-ship-it <amulyamunnur3@gmail.com>
2026-07-28 11:46:58 +0530

    added like button

--- REACTJS/my-first-react-app/src/App.jsx
+++ REACTJS/my-first-react-app/src/App.jsx
@@ -1,41 +1,22 @@
-import Employee from "./Employee";
-
+import { useState } from "react";
 function App() {
+  const [likes, setLikes] = useState(0);
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+  };
+
+  const handleReset = () => {
+    setLikes(0);
+  };
+
   return (
     <div>
-      <h1>Employee Profile Cards</h1>
+      <h1>Like Button Application</h1>
+      <h2>Likes: {likes}</h2>
 
-      <Employee 
-        name="Ramesh"
-        id="EMP101"
-        department="AIML"
-        designation="Assistant Professor"
-        salary="₹55,000"
-      />
-
-      <Employee 
-        name="Kavya"
-        id="EMP102"
-        department="CSE"
-        designation="Associate Professor"
-        salary="₹62,000"
-      />
-
-      <Employee 
-        name="Arjun"
-        id="EMP103"
-        department="ECE"
-        designation="Professor"
-        salary="₹70,000"
-      />
-
-      <Employee 
-        name="Sneha"
-        id="EMP104"
-        department="IT"
-        designation="Lecturer"
-        salary="₹45,000"
-      />
+      <button onClick={handleLike}>Like 👍</button>
+      <button onClick={handleReset}>Reset 🔄</button>
     </div>
   );
 }
